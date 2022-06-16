72d6faf8b80078668f58888168edd016b03a0486
Andrey Peshko <coursestud@mail.ru>
2022-06-16 20:24:48 +0300

    feat: add declare

---
+++ declare.d.ts
@@ -0,0 +1,11 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import { SendData } from './src/types/type';
+
+declare module 'http' {
+  export interface IncomingMessage {
+    param?: string;
+  }
+  export interface ServerResponse {
+    send: (status: number, data: SendData) => void;
+  }
+}
