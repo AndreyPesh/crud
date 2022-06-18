c623596846d61915d9a8097c57a3337f98b9dbdf
AndreyPesh <coursestud@mail.ru>
2022-06-18 21:29:15 +0300

    fix: change name host env

--- src/middleware/parseUrl.ts
+++ src/middleware/parseUrl.ts
@@ -2,7 +2,7 @@ import { IncomingMessage } from 'http';
 import { MAX_PARAMETER_NUMBER } from '../types/constant';
 
 export const parseUrl = (req: IncomingMessage) => {
-  const fullUrl = (`${process.env.HOSTNAME}:${process.env.PORT}` + req.url) as string;
+  const fullUrl = (`${process.env.HOSTNAME_SERVER}:${process.env.PORT}` + req.url) as string;
   const url = new URL(fullUrl);
   const allParameters = new URL(url).pathname.split('/');
   if (allParameters.length > MAX_PARAMETER_NUMBER) {
